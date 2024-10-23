de58c5e6c644b892246172da63903214085ae1ae
ffbccc <102197565+ffbccc@users.noreply.github.com>
2024-10-24 01:47:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 // Функція для завантаження вмісту текстових файлів
 async function loadContentFromFile(file) {
-    const response = await fetch(file);
-    const text = await response.text();
-    const lines = text.split('\n').map(line => line.trim()).filter(line => line.length > 0);
+    const fileUrl = `https://ffbccc.github.io/PHILMSUA/${file}`;
+    
+    const response = await fetch(fileUrl);
+    if (response.ok) {
+        const text = await response.text();
+        const lines = text.split('\n').map(line => line.trim()).filter(line => line.length > 0);
 
-    let contentDiv = document.getElementById('content');
-    contentDiv.innerHTML = ''; // Очищуємо попередній контент
+        let contentDiv = document.getElementById('content');
+        contentDiv.innerHTML = ''; // Очищуємо попередній контент
 
-    lines.forEach(line => {
-        const [title, link] = line.split(' - '); // Розділяємо назву та посилання
-        const itemDiv = document.createElement('div');
-        itemDiv.innerHTML = `
-            <div>
-                <h3>${title}</h3>
-            </div>
-        `;
-        contentDiv.appendChild(itemDiv);
-    });
+        lines.forEach(line => {
+            const [title] = line.split(' - '); // Отримуємо лише назву (без посилання)
+            const itemDiv = document.createElement('div');
+            itemDiv.innerHTML = `<h3>${title}</h3>`;
+            contentDiv.appendChild(itemDiv);
+        });
+    } else {
+        console.error('Помилка завантаження файлу:', response.status);
+    }
 }
 
 // Завантаження контенту для кожної вкладки з відповідного файлу
